97bf653ca2d90c4b0131b465d809b3250375b53d
Rafael <rafaelgonzag4@gmail.com>
2024-09-03 19:34:25 -0300

    Feat. ChatMessage

---
+++ Backend/Chat/src/main/java/com/chatforum/chat/chat/ChatMessageService.java
@@ -0,0 +1,30 @@
+package com.chatforum.chat.chat;
+
+import com.chatforum.chat.chatroom.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+    private ChatMessageRepository repository;
+    private ChatRoomService chatRoomService;
+
+    public ChatMessage save(ChatMessage chatMessage) {
+        var chatId = chatRoomService.getChatRoomId(
+                chatMessage.getSenderId(),
+                chatMessage.getRecipientId(),
+                true).orElseThrow();
+        chatMessage.setChatId(chatId);
+        repository.save(chatMessage);
+        return chatMessage;
+    }
+
+    public List<ChatMessage> findChatMessages(String senderId, String recipientId) {
+        var chatId = chatRoomService.getChatRoomId(senderId, recipientId, false);
+        return chatId.map(repository::findByChatId).orElse(new ArrayList<>());
+    }
+}
